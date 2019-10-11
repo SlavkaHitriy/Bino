14e8849f5889c131a07efb14457a9d382aa6bfff
Slavka <slavik.nukutuk@gmail.com>
2019-10-11 22:26:49 +0300

    full site without adaptive and full animation

--- app/js/main.js
+++ app/js/main.js
@@ -19,5 +19,23 @@ $(function(){
       dotsClass: 'services__dots',
   });
 
+   $('.study__slider').slick({
+      arrows: false,
+      dots: true,
+      dotsClass: 'study__dots',
+   });
+
+
+   $('.works__wrapper .tab').on('click', function (event) {
+      var id = $(this).attr('data-id');
+      $('.works__wrapper').find('.tab-item').removeClass('active-tab').hide();
+      $('.works__wrapper .tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#' + id).addClass('active-tab').fadeIn();
+      return false;
+   });
+
+
+   var mixer = mixitup('.works__content');
 
 });
